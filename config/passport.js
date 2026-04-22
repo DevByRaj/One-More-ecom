9768107a907eac60d457302503bceaf105ba910f
Rajmohan <rajmohan0816@gmail.com>
2026-04-22 21:10:55 +0530

    feat:loginGoogleAuth updated

--- config/passport.js
+++ config/passport.js
@@ -1,73 +1,65 @@
-
-import passport from "passport"
-import { Strategy  as GoogleStrategy} from "passport-google-oauth20"
-import User from "../models/userModel.js"
-import { sendOtpEmail } from "../services/mailService.js";
-
-
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import User from "../models/userModel.js";
 
 passport.use(
-    new GoogleStrategy(
-        {
-            clientID: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL:"http://localhost:3000/auth/google/callback",
-            
-        },
-        async(accessToken, refreshToken, profile, done) =>{
-            try {
-                const email = profile.emails[0].value;
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const email = profile.emails[0].value;
 
-                if(!email){
-                    return done(new Error("No email found from Google"), null)
-                }
-                let user = await User.findOne({email})
-
-                const otp = Math.floor(1000 + Math.random() * 9000).toString()
-
-                if(!user){
-                    user = new User({
-                        name: profile.displayName,
-                        email,
-                        authType: "google",
-                        isVerified: false,
-                        otp,
-                        otpExpires: Date.now()+ 2 * 60 * 1000
-                    })
-
-                    await user.save()
-                    await sendOtpEmail(email, otp)
-                } else {
-                    
-                    if(user.isVerified){
-                        return done(null, user)
-                    }
-
-                    const otp = Math.floor(1000+Math.random()*9000).toString()
-
-                    user.otp = otp
-                    user.otpExpires = Date.now()+2*60*1000
-
-                    await user.save()
-
-                await sendOtpEmail(email,otp)
-
-                }
-                
-                return done(null, user)
-                
-            } catch (error) {
-                return done(error, null)
-            }
+        if (!email) {
+          return done(new Error("No email from Google"), null)
         }
-    )
-)
 
-passport.serializeUser((user, done) =>{
-    done(null, user.id)
-})
+        let user = await User.findOne({ email })
 
-passport.deserializeUser(async(id, done) => {
-    const user= await User.findById(id)
+        if (user) {
+
+          if (user.authType !== "google") {
+            return done(null, false, {
+              message: "Please login using email & password",
+            });
+          }
+
+          return done(null, user);
+        }
+
+        user = new User({
+          name: profile.displayName,
+          email,
+          authType: "google",
+          isVerified: true,
+          profileImage: profile.photos?.[0]?.value || ""
+        });
+
+        await user.save()
+
+        return done(null, user)
+
+      } catch (error) {
+        return done(error, null)
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id)
     done(null, user)
-})
+  } catch (error) {
+    done(error, null)
+  }
+});
+
+export default passport
