12b5f546f7821fc25b15b9c70ff5c38f0ef3f447
Rajmohan <rajmohan0816@gmail.com>
2026-05-07 00:56:43 +0530

    bug fix

--- models/userModel.js
+++ models/userModel.js
@@ -2,61 +2,63 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
     {
-     name: {
-      type: String,
-      required: true,
-      trim: true,
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            // match: [/^[a-zA-Z0-9._%+-]+@gmail\.com$/, "Require valid email"],
+        },
+        password: {
+            type: String,
+            required: false,
+        },
+        phone: {
+            type: String,
+            default: "",
+        },
+        profileImage: {
+            type: String,
+            default: ""
+        },
+        isBlocked: {
+            type: Boolean,
+            default: false,
+        },
+        isVerified: {
+            type: Boolean,
+            default: false,
+        },
+        authType: {
+            type: String,
+            enum: ["local", "google"],
+            default: "local",
+        },
+        otp: {
+            type: String,
+        },
+
+        otpExpires: {
+            type: Date,
+        },
+        
+        resetToken: {
+            type: String,
+        },
+        resetTokenExpires: {
+            type: Date,
+        },
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        // match: [/^[a-zA-Z0-9._%+-]+@gmail\.com$/, "Require valid email"],
-    },
-    password: {
-        type: String,
-        required: false,
-    },
-    phone: {
-        type: String,
-        default: "",
-    },
-    profileImage:{
-        type: String,
-        default:""
-    },
-    isBlocked: {
-        type: Boolean,
-        default: false,
-    },
-    isVerified: {
-        type: Boolean,
-        default: false,
-    },
-    authType: {
-        type: String,
-        enum:["local", "google"],
-        default: "local",
-    },
-    otp: {
-        type: String,
-    },
-    otpExpires:{
-        type: Date,
-    },
-    resetToken:{
-        type: String,
-    },
-    resetTokenExpires:{
-        type: Date,
-    },
-},
-{
-    timestamps: true,
-}
+    {
+        timestamps: true,
+    }
 )
 
-const User=mongoose.model("user", userSchema)
+const User = mongoose.model("user", userSchema)
 
 export default User
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,6 +5,7 @@ import {
   getSignup, postSignup,
   getLogout,
   getProfile, getEditProfile, postEditProfile,
+  getVerifyOTP,
   verifyOTP,
   setDefaultAddress,
   getForgotPassword,
@@ -69,34 +70,35 @@ router.get("/auth/google/callback",
   }
 );
 
+router.get("/verify-otp", getVerifyOTP)
 
-router.get("/verify-otp", (req, res) => {
-  const { email, type } = req.query;
+// router.get("/verify-otp", (req, res) => {
+//   const { email, type } = req.query;
 
-  if (type === "forgot" && req.session.resetDone) {
-    return res.redirect(`/reset-password?email=${req.query.email}`)
-  }
+//   if (type === "forgot" && req.session.resetDone) {
+//     return res.redirect(`/reset-password?email=${req.query.email}`)
+//   }
 
-  let finalEmail = email
+//   let finalEmail = email
 
-  if (type === "emailEdit") {
-    if (!req.session.emailEdit) {
-      return res.redirect("/profile")
-    }
-    finalEmail = req.session.emailEdit.newEmail
-  }
+//   if (type === "emailEdit") {
+//     if (!req.session.emailEdit) {
+//       return res.redirect("/profile")
+//     }
+//     finalEmail = req.session.emailEdit.newEmail
+//   }
 
 
-  if (!finalEmail) {
-    return res.redirect("/signup")
-  }
+//   if (!finalEmail) {
+//     return res.redirect("/signup")
+//   }
 
-  res.render("user/verifyOtp", {
-    email: finalEmail,
-    error: null,
-    type
-  })
-})
+//   res.render("user/verifyOtp", {
+//     email: finalEmail,
+//     error: null,
+//     type
+//   })
+// })
 
 router.post("/verify-otp", verifyOTP)
 
